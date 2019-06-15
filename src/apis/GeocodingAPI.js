4cb657bb8caab2758d65a3d0fa89ffeb3ec9732e
Diego Rocha Chagas <drochachagas@gmail.com>
2019-06-15 01:17:47 -0300

    Deploy

--- src/apis/GeocodingAPI.js
+++
@@ -1,7 +0,0 @@
-import Geocode from 'react-geocode';
-export const API_KEY = "AIzaSyAHcixvHTRnP4vJCL1mkav-oY8wPJf4Fgc";
-
-Geocode.setApiKey(API_KEY);
-
-export const GeocodingAPI = async CEP => await Geocode.fromAddress(CEP);
-
--- src/apis/ViaCEPAPI.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const ViaCEPAPI = async CEP => await axios.get(`https://viacep.com.br/ws/${CEP}/json/`);
-
-export default ViaCEPAPI;
