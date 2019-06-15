c7b5def7529a43769734afece943419887476adf
Diego Rocha Chagas <drochachagas@gmail.com>
2019-06-15 01:14:53 -0300

    Updates

---
+++ precache-manifest.4356c4fa22ee299ac933368bc6d201fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af71ffa34e155ca49bc64c41b836670",
+    "url": "/correios/index.html"
+  },
+  {
+    "revision": "04e4d66ff73d20864b03",
+    "url": "/correios/static/css/main.13ea3829.chunk.css"
+  },
+  {
+    "revision": "2e4b640d444d2030209e",
+    "url": "/correios/static/js/2.98dd2dce.chunk.js"
+  },
+  {
+    "revision": "04e4d66ff73d20864b03",
+    "url": "/correios/static/js/main.2d990dc2.chunk.js"
+  },
+  {
+    "revision": "fb011cdea90830bfe211",
+    "url": "/correios/static/js/runtime~main.dcebcf6f.js"
+  }
+]);
